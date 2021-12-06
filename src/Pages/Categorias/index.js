9c25386378675de3dcf0ce22beb0e47fb2093319
david115x0 <davidsilvafs29@gmail.com>
2021-12-06 17:57:20 -0300

    Teste com o id na pesquisa

--- src/Pages/Categorias/index.js
+++ src/Pages/Categorias/index.js
@@ -1,38 +1,51 @@
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import { useEffect, useState } from 'react';
+import { Component } from 'react';
+/* import { Link } from 'react-router-dom'; */
+import { Formik, Form, Field } from 'formik';
 import Swal from 'sweetalert2';
-import api from '../../services/index'
+import api from '../../services/index';
 
+export default class Categorias extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categoria: {}
+    }
+  }
 
-const Categorias = () => {
-    const [categorias, setCategorias] = useState([]);
-    useEffect(() => {
-        try {
-            const response = api.get(`categorias`);
-            setCategorias(response.data);
-            console.log(response)
-        } catch (error) {
-            Swal.fire({
-                title: error.response.status,
-                icon: 'error',
-                text: error.response.data.message
-            });
-        }
-    }, [])
+  handleSubmit = async ({ nomeCategoria }) => {
+    try {
+      const response = await api.get(`/categorias/${nomeCategoria}`);
+      this.setState({ categoria: response.data });
+      console.log(response.data)
+    } catch (error) {
+      Swal.fire({
+        title: error.response.status,
+        icon: 'error',
+        text: error.response.data.message
+      });
+    }
+  }
 
+  render() {
     return (
-        <>
-            <Header />
-            {categorias.nome && (
-                <main className="text-center">
-                    <h1>Categorias</h1>
-                    <li>{categorias.nome}</li>
-                </main>
+      <>
+        <main>
+          <div className="col-md-4 col-sm-6 my-3 container text-center">
+            <h2>Categorias</h2>
+            <Formik initialValues={{ nomeCategoria: '' }} onSubmit={this.handleSubmit}>
+              <Form>
+                <Field placeholder="Inserir categoria" required type="text" name="nomeCategoria" id="nomeCategoria" className="form-control my-3" />
+                <button type="submit" className="btn btn-primary">Pesquisar</button>
+              </Form>
+            </Formik>
+            {this.state.categoria && (
+              <ul className="list-group my-3">
+                <li>{this.state.categoria.nome}</li>
+              </ul>
             )}
-            <Footer />
-        </>
-    )
+          </div>
+        </main>
+      </>
+    );
+  }
 }
-
-export default Categorias;
