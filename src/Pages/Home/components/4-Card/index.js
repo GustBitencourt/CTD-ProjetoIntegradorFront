1041e96e4b405b2139e3efff22d40395863eb8fb
david115x0 <davidsilvafs29@gmail.com>
2021-12-06 21:21:22 -0300

    Home Card

--- src/Pages/Home/components/4-Card/index.js
+++ src/Pages/Home/components/4-Card/index.js
@@ -1,28 +1,29 @@
 import { Card, Row, Col } from 'react-bootstrap';
 import './style.scss';
 
-import Ex1 from '../../../../images/ex1.jpg';
+
 
 const Card4 = () => {
-
     return (
         <>
-            <Row xs={1} md={2} className="g-4">
-                {Array.from({ length: 4 }).map((_, idx) => (
-                    <Col>
-                        <Card>
-                            <Card.Img variant="top" src={Ex1} />
-                            <Card.Body>
-                                <Card.Title>Card title</Card.Title>
-                                <Card.Text>
-                                    This is a longer card with supporting text below as a natural
-                                    lead-in to additional content. This content is a little bit longer.
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                ))}
-            </Row>
+            
+                <Row xs={1} md={2} className="g-4">
+                    {Array.from({ length: 4 }).map((_, idx) => (
+                        <Col>
+                            <Card >
+                                <Card.Img variant="top"  />
+                                <Card.Body>
+                                    <Card.Title></Card.Title>
+                                    <Card.Text>
+                                        
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+
+                        </Col>
+                    ))}
+                </Row>
+            
         </>
     )
 }
--- src/Pages/Home/components/Card/index.js
+++ src/Pages/Home/components/Card/index.js
@@ -1,53 +1,52 @@
 import { Card, Button, CardGroup } from 'react-bootstrap';
 import './style.scss';
+import Swal from 'sweetalert2';
+import api from '../../../../services/index';
+import { useEffect, useState } from 'react';
 
-import Ex1 from '../../../../images/ex1.jpg';
-import Ex2 from '../../../../images/ex2.jpg';
-import Ex3 from '../../../../images/ex3.jpg';
+/* import Ex1 from '../../../../images/ex1.jpg'; */
+/* import Ex2 from '../../../../images/ex2.jpg';
+import Ex3 from '../../../../images/ex3.jpg'; */
 
 
 const Caard = () => {
+    const [card, setCard] = useState([]);
+
+    useEffect(() => {
+        async function loadCard() {
+            try {
+                const response = await api.get(`/produtos`);
+                console.log(response.data);
+                setCard(response.data);
+            } catch (error) {
+                Swal.fire({
+                    title: error.response.status,
+                    icon: 'error',
+                    text: error.response.data.message
+                })
+            }
+        }
+        loadCard();
+    }, [])
 
     return (
         <>
             <div className="container">
-                <CardGroup>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src={Ex1} />
-                        <Card.Body>
-                            <Card.Title>Front-End</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-                            </Card.Text>
-                            <Button variant="primary">Comprar</Button>
-                        </Card.Body>
-                    </Card>
-
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src={Ex2} />
-                        <Card.Body>
-                            <Card.Title>Ux/Ui</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-                            </Card.Text>
-                            <Button variant="primary">Comprar</Button>
-                        </Card.Body>
-                    </Card>
-
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src={Ex3}/>
-                        <Card.Body>
-                            <Card.Title>Back-end</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-                            </Card.Text>
-                            <Button variant="primary">Comprar</Button>
-                        </Card.Body>
-                    </Card>
-                </CardGroup>
+                {card.map(c => (
+                    <CardGroup>
+                        <Card style={{ width: '18rem' }} key={c.id}>
+                            <Card.Img variant="top" src={c.imagem} />
+                            <Card.Body>
+                                <Card.Title>{c.titulo}</Card.Title>
+                                <Card.Text>
+                                    {c.descricao}
+                                </Card.Text>
+                                <Button variant="primary">Comprar</Button>
+                            </Card.Body>
+                        </Card>
+                        
+                    </CardGroup>
+                ))}
             </div>
         </>
     )
