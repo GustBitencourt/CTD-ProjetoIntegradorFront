c4ec44eaff7e0525fbe996ab945b1acf7ec021fd
david115x0 <davidsilvafs29@gmail.com>
2021-12-08 18:15:27 -0300

    links do backend

--- src/services/index.js
+++ src/services/index.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'http://turma3g2commerce-env.eba-gt5mkcww.us-east-1.elasticbeanstalk.com'
-  /* https://dhcommerce.herokuapp.com/ */
+  // https://dhcommerce.herokuapp.com
+  // http://turma3g2commerce-env.eba-gt5mkcww.us-east-1.elasticbeanstalk.com
 });
 
 export default api;
