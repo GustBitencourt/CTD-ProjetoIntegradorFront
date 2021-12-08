bbd18dd7787e9e0144d011a7f12dbe1258b12930
david115x0 <davidsilvafs29@gmail.com>
2021-12-08 09:50:12 -0300

    categorias

--- src/Pages/Categorias/index.js
+++ src/Pages/Categorias/index.js
@@ -2,6 +2,8 @@
 import Swal from 'sweetalert2';
 import api from '../../services/index';
 import { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 const Categorias = () => {
   const [categoria, setCategoria] = useState([]);
@@ -9,7 +11,7 @@ const Categorias = () => {
   useEffect(() => {
     async function loadCategoria() {
       try {
-        const response = await api.get(`/categorias`);
+        const response = await api.get(`/produtos/categorias`);
         console.log(response.data);
         setCategoria(response.data);
       } catch (error) {
@@ -26,14 +28,17 @@ const Categorias = () => {
 
   return (
     <>
-    <div>
-    {categoria.map(d => (
-      <ul className="col-xl-4 col-lg-6 col-6" key={d.id}>
-        <li>Nome: {d.nome}</li>
-      </ul>
-    
-    ))} 
-    </div>
+
+      <div>
+        {categoria.map(nomeCategoria => (
+          /*       <ul className="col-xl-4 col-lg-6 col-6" key={d.id}>
+                  <li>Nome: {d.nome}</li>
+                </ul> */
+          <Button variant="primary" as={Link} to={`/cursos/${nomeCategoria}`} className="button" key={nomeCategoria}>
+           Categoria : {nomeCategoria}
+          </Button>
+        ))}
+      </div>
     </>
   );
 
--- src/Pages/Produtos/index.js
+++ src/Pages/Produtos/index.js
@@ -3,23 +3,13 @@ import { Helmet } from 'react-helmet';
 import Swal from 'sweetalert2';
 import api from '../../services/index';
 import { useEffect, useState } from 'react';
-import { Container,  Form} from 'react-bootstrap';
+import { Container, Form } from 'react-bootstrap';
 
 
 
 const Produtos = () => {
   const [produtos, setProdutos] = useState([]);
-  const [filteredCategoria, setFilteredCategoria] = useState(produtos);
 
-  const handleSearch = (event) => {
-    let value = event.target.value.toLowerCase();
-    let result = [];
-    console.log(value);
-    result = produtos.filter((data) => {
-      return data.categoria.nome.search(value) !== -1;
-    });
-    setFilteredCategoria(result);
-  }
 
   useEffect(() => {
     async function loadProdutos() {
@@ -27,7 +17,6 @@ const Produtos = () => {
         const response = await api.get(`/produtos`);
         console.log(response.data);
         setProdutos(response.data);
-        setFilteredCategoria(response.data);
       } catch (error) {
         Swal.fire({
           title: error.response.status,
@@ -47,27 +36,16 @@ const Produtos = () => {
           CTD - Educational | Cursos
         </title>
       </Helmet>
-
-
-
       <Container>
         <Container>
           <h2>Cursos</h2>
         </Container>
-
-
-
-        {filteredCategoria.map((value, index) => {
-          return (
-            <Form.Select aria-label="Default select example" key={value.id} onChange={(event) => handleSearch(event)}>
-              <option>Escolha a Categoria</option>
-              <option >{value.categoria.nome}</option>
-            </Form.Select>
-          )
-        })}
+        <Form.Select aria-label="Default select example">
+          <option value="">frontend</option>
+          <option value="">backend</option>
+          <option value="">desing</option>
+        </Form.Select>
       </Container>
-
-
       <Container>
         {produtos.map(curso => (
           <ul className="col-xl-4 col-lg-6 col-6" key={curso.id}>
