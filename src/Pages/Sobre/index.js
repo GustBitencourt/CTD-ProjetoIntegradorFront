e30d56136e4c1cf0367d0b8de7eb15eb99b1d1ee
FELIPE RODRIGUES DE MELO <85718153+Felipe00007@users.noreply.github.com>
2021-12-10 10:10:43 -0300

    Sobre

--- src/Pages/Sobre/index.js
+++ src/Pages/Sobre/index.js
@@ -19,8 +19,11 @@ const Sobrenos = () => {
             <Card.Text>
                Front-End
             </Card.Text>
-            <a href="https://portifolio-fyyq71vhd-felipe00007.vercel.app/">
-            <Button variant="primary">Acessar Portfolio</Button>
+            <a href="https://github.com/Felipe00007">
+            <img className="git" src="https://cdn-icons-png.flaticon.com/512/25/25231.png"></img>
+            </a>
+            <a href="https://www.linkedin.com/in/felipe-rodrigues-de-melo-b07a35141/">
+            <img className="linkedin" src="https://t.ctcdn.com.br/o6YUYZNxhn1ob0FEjQo-a7KapYU=/400x400/smart/i490027.jpeg"></img>
             </a>
           </Card.Body>
         </Card>
@@ -35,7 +38,10 @@ const Sobrenos = () => {
                 Front-End
             </Card.Text>
             <a href="https://github.com/GustBitencourt">
-            <Button variant="primary">Acessar Portfolio</Button>
+            <img className="git" src="https://cdn-icons-png.flaticon.com/512/25/25231.png"></img>
+            </a>
+            <a href="https://www.linkedin.com/in/gustavo-bitencourt-081615204/">
+            <img className="linkedin" src="https://t.ctcdn.com.br/o6YUYZNxhn1ob0FEjQo-a7KapYU=/400x400/smart/i490027.jpeg"></img>
             </a>
           </Card.Body>
         </Card>
@@ -50,12 +56,15 @@ const Sobrenos = () => {
                Full Stack
             </Card.Text>
             <a href="https://github.com/david115x0">
-            <Button variant="primary">Acessar Portfolio</Button>
+            <img className="git" src="https://cdn-icons-png.flaticon.com/512/25/25231.png"></img>
+            </a>
+            <a href="https://www.linkedin.com/in/david-silva-sousa-790a42225/">
+            <img className="linkedin" src="https://t.ctcdn.com.br/o6YUYZNxhn1ob0FEjQo-a7KapYU=/400x400/smart/i490027.jpeg"></img>
             </a>
           </Card.Body>
         </Card>
         <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Img variant="top" src="https://media-exp1.licdn.com/dms/image/C4D03AQEmgThrgPkJ8g/profile-displayphoto-shrink_800_800/0/1633190458680?e=1644451200&v=beta&t=8Lhv00MkjTXHgIbpmAlUFQZl56dGKxe_v0MhyR7Kshw" />
           <Card.Body>
             <Card.Title>Ruan Araújo</Card.Title>
             <Card.Text>
@@ -65,24 +74,30 @@ const Sobrenos = () => {
               Back-End
             </Card.Text>
            
-            <a href="https://www.linkedin.com/in/felipe-rodrigues-de-melo-a9a8b5a4/">
-            <Button variant="primary">Acessar Portfolio</Button>
+            <a href="https://github.com/Ruan-Araujo">
+            <img className="git" src="https://cdn-icons-png.flaticon.com/512/25/25231.png"></img>
+            </a>
+            <a href="https://www.linkedin.com/in/ruan-damacena/">
+            <img className="linkedin" src="https://t.ctcdn.com.br/o6YUYZNxhn1ob0FEjQo-a7KapYU=/400x400/smart/i490027.jpeg"></img>
             </a>
           </Card.Body>
         </Card>
         <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Img variant="top" src="https://media-exp1.licdn.com/dms/image/D4E35AQHOnYWunabwVg/profile-framedphoto-shrink_800_800/0/1636069371339?e=1639231200&v=beta&t=Uuv75tpEYddKtmrlCkF8pRMLHRmxzke4FfytSdhnpTA" />
           <Card.Body>
             <Card.Title>Felipe S.Araujo</Card.Title>
             <Card.Text>
-             Desenvolvedor:
+             Responsalvel Por:
             </Card.Text>
             <Card.Text>
-              Front-End
+              Designer
             </Card.Text>
            
-            <a href="https://www.linkedin.com/in/felipe-rodrigues-de-melo-a9a8b5a4/">
-            <Button variant="primary">Acessar Portfolio</Button>
+            <a href="">
+            <img className="git" src="https://cdn-icons-png.flaticon.com/512/25/25231.png"></img>
+            </a>
+            <a href="https://www.linkedin.com/in/felipemotion/">
+            <img className="linkedin" src="https://t.ctcdn.com.br/o6YUYZNxhn1ob0FEjQo-a7KapYU=/400x400/smart/i490027.jpeg"></img>
             </a>
           </Card.Body>
         </Card>
