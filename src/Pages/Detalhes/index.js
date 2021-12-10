40e94553b6ec949fcf227bf41a321e37cd0b1423
FELIPE RODRIGUES DE MELO <85718153+Felipe00007@users.noreply.github.com>
2021-12-10 11:01:25 -0300

    Detalhes

--- src/Pages/Detalhes/index.js
+++ src/Pages/Detalhes/index.js
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 import { CursoContext } from '../../contexts/CursoContext';
 import api from '../../services/index';
 import Swal from 'sweetalert2';
+import './style.scss';
 
 
 
@@ -39,19 +40,20 @@ const Detalhes = () => {
       <Helmet>
         <title>CTD - Educational | {cursos[0] ? cursos[0].titulo : 'Curso'}</title>
       </Helmet>
+      <div className="Detalhes">
+        {cursos.map(produtos => (
+          <ListGroup as="ul" className="Flex col-xl-4 col-lg-6 col-6" key={produtos.id}>
 
-      {cursos.map(produtos =>(
-        <ListGroup as="ul" className="col-xl-4 col-lg-6 col-6" key={produtos.id}>
-
-          <ListGroup.Item as="li"><img src={produtos.imagem} alt={`Foto do ${produtos.titulo}`} title={produtos.titulo} /></ListGroup.Item>
-          <ListGroup.Item as="li">Titulo: {produtos.titulo}</ListGroup.Item>
-          <ListGroup.Item as="li">Preço: {produtos.preco}</ListGroup.Item>
-          <ListGroup.Item as="li">Descrição: {produtos.descricao}</ListGroup.Item>
-          <ListGroup.Item as="li">Categoria: {produtos.categoria.nome}</ListGroup.Item>
-          <Button to={`/carrinho`} as={Link}>Adicionar ao Carrinho</Button>
-        </ListGroup>
-      ))}
-    </>
-  )
+            <ListGroup.Item as="li"><img className="Curso" src={produtos.imagem} alt={`Foto do ${produtos.titulo}`} title={produtos.titulo} /></ListGroup.Item>
+            <ListGroup.Item as="li">Titulo: {produtos.titulo}</ListGroup.Item>
+            <ListGroup.Item as="li">Preço: {produtos.preco}</ListGroup.Item>
+            <ListGroup.Item as="li">Descrição: {produtos.descricao}</ListGroup.Item>
+            <ListGroup.Item as="li">Categoria: {produtos.categoria.nome}</ListGroup.Item>
+            <Button className="btn" to={`/carrinho`} as={Link}>Adicionar ao Carrinho</Button>
+          </ListGroup>
+        ))}
+       </div>
+      </>
+      )
 }
-export default Detalhes;
+      export default Detalhes;
